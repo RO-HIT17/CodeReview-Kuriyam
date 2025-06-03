886eb8555345e2ac4b70692964b13a98c5374c2d
rohit <rohit.selvakumaran@gmail.com>
2025-06-03 13:50:29 +0530

    test 2

--- code-review-cli/test_script.py
+++ code-review-cli/test_script.py
@@ -1,7 +1,9 @@
-# test_script1.py
+def has_duplicates(arr):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] == arr[j]:
+                return True
+    return False
 
-def add_numbers(a,b):
-    sum = a+b
-    print("Sum is "+str(sum))
-
-add_numbers(5,10)
+nums = [1, 2, 3, 4, 5, 6, 2]
+print("Contains duplicates:", has_duplicates(nums))
