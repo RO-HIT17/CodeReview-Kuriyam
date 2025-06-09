00b7a711d9115f04f680d2fc3a0a22dd1c0938df
rohit <rohit.selvakumaran@gmail.com>
2025-06-10 01:46:00 +0530

    finetuned new model

--- code-review-cli/scripts/review.py
+++ code-review-cli/scripts/review.py
@@ -3,7 +3,7 @@ import requests
 import time
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
-MODEL_NAME = "finetuned_codellama"  
+MODEL_NAME = "codellama_cyber"  
 
 def read_file(file_path):
     """
