b7586719d0bed70a908eb3239e852e5ffbaa2c5e
rohit <rohit.selvakumaran@gmail.com>
2025-06-24 02:56:44 +0530

    code dump

--- server/main.py
+++ server/main.py
@@ -4,8 +4,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.github import router as github_router
 from feedback.routes import router as feedback_router
 from services.issue_mapper import router as issue_mapper_router  # new
+from api.bitbucket import router as bitbucket_router
+
 
 app = FastAPI(title="Code Review Platform")
+# main.py
+
 
 # CORS if needed
 app.add_middleware(
@@ -20,3 +24,4 @@ app.add_middleware(
 app.include_router(github_router, prefix="/github", tags=["GitHub"])
 app.include_router(feedback_router, tags=["Feedback"])
 app.include_router(issue_mapper_router, prefix="/issue-check", tags=["Issue Validation"])
+app.include_router(bitbucket_router, prefix="/bitbucket", tags=["Bitbucket"])
