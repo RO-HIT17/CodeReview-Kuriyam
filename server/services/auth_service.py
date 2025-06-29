31fb3b7dee30b8e2d002074e8c56f7594f21616d
rohit <rohit.selvakumaran@gmail.com>
2025-06-30 01:13:16 +0530

    add backend auth

---
+++ server/services/auth_service.py
@@ -0,0 +1,17 @@
+from db import models, database
+from utils.hash import hash_password, verify_password
+from sqlalchemy.orm import Session
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, email: str, password: str, is_admin=False):
+    user = models.User(
+        email=email,
+        hashed_password=hash_password(password),
+        is_admin=is_admin
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
