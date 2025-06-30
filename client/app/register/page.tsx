a3808bdd6b3ebbcab306cff0bc30589dd28e71a0
rohit <rohit.selvakumaran@gmail.com>
2025-07-01 02:03:41 +0530

    fix urls

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -30,7 +30,7 @@ export default function RegisterPage() {
     }
 
     try {
-      const response = await fetch("http://localhost:8000/auth/register", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
